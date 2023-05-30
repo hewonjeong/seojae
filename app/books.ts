343e371a6a81db4ab09c1037bcf6c997ab1a13e5
Hewon Jeong <hewon.jng@gmail.com>
2023-05-30 15:43:01 +0900

    chore: add books data

---
+++ app/books.ts
@@ -0,0 +1,76 @@
+const books = [
+  {
+    title: '함께 자라기',
+    authors: ['김창준'],
+    url: 'http://www.yes24.com/Product/Goods/67350256',
+    image: '/67350256.jpeg',
+  },
+  {
+    title: '지적 대화를 위한 넓은 지식1',
+    authors: ['채사장'],
+    url: 'http://www.yes24.com/Product/Goods/86545658',
+    image: '/86545658.jpeg',
+  },
+  {
+    title: '넛지: 파이널 에디션',
+    authors: ['리처드 탈러', '캐스 선스타인'],
+    url: 'http://www.yes24.com/Product/Goods/110157236',
+    image: '/110157236.jpeg',
+  },
+  {
+    title: '숨',
+    authors: ['테드 창'],
+    url: 'http://www.yes24.com/Product/Goods/108688589',
+    image: '/108688589.jpeg',
+  },
+  {
+    title: '습관의 힘',
+    authors: ['찰스 두히그'],
+    url: 'http://www.yes24.com/Product/Goods/7950702',
+    image: '/7950702.jpeg',
+  },
+  {
+    title: '워런 버핏의 주주 서한',
+    authors: ['워런 버핏', '로렌스 커닝햄'],
+    url: 'http://www.yes24.com/Product/Goods/110878515',
+    image: '/110878515.jpeg',
+  },
+  {
+    title: '호밀밭의 파수꾼',
+    authors: ['J.D. 샐린저'],
+    url: 'http://www.yes24.com/Product/Goods/204300',
+    image: '/204300.jpeg',
+  },
+  {
+    title: '전설로 떠나는 월가의 영웅',
+    authors: ['피터 린치', '존 로스차일드'],
+    url: 'http://www.yes24.com/Product/Goods/102951831',
+    image: '/102951831.jpeg',
+  },
+  {
+    title: '시작의 기술',
+    authors: ['개리 비숍'],
+    url: 'http://www.yes24.com/Product/Goods/72127469',
+    image: '/72127469.jpeg',
+  },
+  {
+    title: '프로젝트 헤일메리',
+    authors: ['앤디 위어'],
+    url: 'http://www.yes24.com/Product/Goods/101375755',
+    image: '/101375755.jpeg',
+  },
+  {
+    title: '실용주의 프로그래머',
+    authors: ['앤드류 헌트', '데이비드 토머스'],
+    url: 'http://www.yes24.com/Product/Goods/12501565',
+    image: '/12501565.jpeg',
+  },
+  {
+    title: '리팩터링 2판',
+    authors: ['마틴 파울러'],
+    url: 'http://www.yes24.com/Product/Goods/89649360',
+    image: '/89649360.jpeg',
+  },
+]
+
+export default books
