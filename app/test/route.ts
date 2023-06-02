e6394db7fb0a91abf6228f72891568b726a0c938
Hewon Jeong <hewon.jng@gmail.com>
2023-06-02 12:35:17 +0900

    log

--- app/test/route.ts
+++ app/test/route.ts
@@ -1,6 +1,21 @@
 import { NextResponse } from 'next/server'
 
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+}
+
 export async function POST(request: Request) {
-  console.log(request)
-  return NextResponse.json({ request: request })
+  const body = await request.json()
+  console.log(body)
+  const keys = request.headers.keys()
+  const headers: any = {}
+  request.headers.forEach((v, k) => {
+    headers[k] = v
+  })
+
+  return NextResponse.json({ request: JSON.stringify(request), body, headers })
 }
