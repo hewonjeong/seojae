08387c0313263a7b9d633997a698a1352c3c813a
Hewon Jeong <hewon.jng@gmail.com>
2023-06-02 12:43:52 +0900

    config

--- app/test/route.ts
+++ app/test/route.ts
@@ -1,13 +1,5 @@
 import { NextResponse } from 'next/server'
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '1mb',
-    },
-  },
-}
-
 export async function POST(request: Request) {
   const body = await request.json()
   const headers: any = {}
