1c210df26cf248411412a3978184c72f5085eb31
Hewon Jeong <hewon.jng@gmail.com>
2023-06-04 17:09:29 +0900

    feat: markup search input

---
+++ app/Icons.tsx
@@ -0,0 +1,56 @@
+import { HTMLProps } from 'react'
+
+type Props = {
+  size?: number
+} & HTMLProps<SVGSVGElement>
+
+function SVG(props: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 512 512"
+      fill="currentColor"
+      width={props.size}
+      height={props.size}
+      {...props}
+    ></svg>
+  )
+}
+
+export function Person(props: Props) {
+  return (
+    <SVG {...props}>
+      <path d="M256 48C141.31 48 48 141.31 48 256s93.31 208 208 208 208-93.31 208-208S370.69 48 256 48zm-50.22 116.82C218.45 151.39 236.28 144 256 144s37.39 7.44 50.11 20.94c12.89 13.68 19.16 32.06 17.68 51.82C320.83 256 290.43 288 256 288s-64.89-32-67.79-71.25c-1.47-19.92 4.79-38.36 17.57-51.93zM256 432a175.49 175.49 0 01-126-53.22 122.91 122.91 0 0135.14-33.44C190.63 329 222.89 320 256 320s65.37 9 90.83 25.34A122.87 122.87 0 01382 378.78 175.45 175.45 0 01256 432z" />
+    </SVG>
+  )
+}
+
+export function Search(props: Props) {
+  return (
+    <SVG {...props}>
+      <path
+        d="M221.09 64a157.09 157.09 0 10157.09 157.09A157.1 157.1 0 00221.09 64z"
+        fill="none"
+        stroke="currentColor"
+        strokeMiterlimit="10"
+        strokeWidth="32"
+      />
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeMiterlimit="10"
+        strokeWidth="32"
+        d="M338.29 338.29L448 448"
+      />
+    </SVG>
+  )
+}
+
+export function Close(props: Props) {
+  return (
+    <SVG {...props}>
+      <path d="M256 48C141.31 48 48 141.31 48 256s93.31 208 208 208 208-93.31 208-208S370.69 48 256 48zm75.31 260.69a16 16 0 11-22.62 22.62L256 278.63l-52.69 52.68a16 16 0 01-22.62-22.62L233.37 256l-52.68-52.69a16 16 0 0122.62-22.62L256 233.37l52.69-52.68a16 16 0 0122.62 22.62L278.63 256z" />
+    </SVG>
+  )
+}
--- app/search/Form.tsx
+++ app/search/Form.tsx
@@ -4,16 +4,13 @@ import { key } from '@/query/key'
 import { useQuery } from '@tanstack/react-query'
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import { Close, Search } from '../Icons'
 
 export default function Form() {
   const [query, setQuery] = useState('')
   const debouncedQuery = useDebounced(query)
 
-  const {
-    data: books,
-    isLoading,
-    isFetching,
-  } = useQuery({
+  const { data: books, isFetching } = useQuery({
     queryKey: [key.books, debouncedQuery],
     queryFn: () => fetchBooks(debouncedQuery),
     enabled: !!debouncedQuery,
@@ -21,46 +18,67 @@ export default function Form() {
   })
 
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
-      <input
-        autoFocus
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      {!books && isFetching && <div>loading...</div>}
-      {query && (
-        <ul>
-          {books?.map((item) => {
-            return (
-              <li
-                key={item.isbn}
-                className="grid grid-cols-[auto_1fr] h-48 relative max-w-full"
-              >
-                <div className="relative w-36">
-                  <Image
-                    fill
-                    sizes="10vw"
-                    className="object-contain"
-                    src={item.image}
-                    alt=""
-                  />
-                </div>
-                <div className="overflow-auto">
-                  <div className="font-semibold truncate">{item.title}</div>
-                  <div className="text-sm text-gray-400 truncate">
-                    {item.author}
+    <form
+      className="max-h-[100vh] grid grid-rows-[auto_1fr]"
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <div className="grid grid-cols-[auto_1fr_auto] bg-gray-200 h-12">
+        <div className="flex items-center p-2">
+          <Search className="w-5 h-5 text-gray-600" />
+        </div>
+        <div>
+          <input
+            autoFocus
+            autoComplete="off"
+            autoCapitalize="off"
+            enterKeyHint="search"
+            className="w-full h-full bg-transparent border-none focus:ring-0 focus:outline-none"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+        </div>
+        {query && (
+          <button className="p-2" onClick={() => setQuery('')}>
+            <Close className="w-5 h-5 text-gray-600" />
+          </button>
+        )}
+      </div>
+      <section className="overflow-auto">
+        {!books && isFetching && <div>loading...</div>}
+        {query && (
+          <ul>
+            {books?.map((item) => {
+              return (
+                <li
+                  key={item.isbn}
+                  className="grid grid-cols-[auto_1fr] h-48 relative max-w-full"
+                >
+                  <div className="relative w-36">
+                    <Image
+                      fill
+                      sizes="10vw"
+                      className="object-contain"
+                      src={item.image}
+                      alt=""
+                    />
                   </div>
-                </div>
-                <div className="absolute h-full flex items-center right-0">
-                  <button className="p-2 border-gray-950 rounded border hover:bg-gray-200">
-                    ➕ 읽은 책에 추가
-                  </button>
-                </div>
-              </li>
-            )
-          })}
-        </ul>
-      )}
+                  <div className="overflow-auto">
+                    <div className="font-semibold truncate">{item.title}</div>
+                    <div className="text-sm text-gray-400 truncate">
+                      {item.author}
+                    </div>
+                  </div>
+                  <div className="absolute h-full flex items-center right-0">
+                    <button className="px-2 py-1 border-gray-950 rounded border hover:bg-gray-200">
+                      ➕ 읽은 책에 추가
+                    </button>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        )}
+      </section>
     </form>
   )
 }
